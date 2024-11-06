a988ff0504e6f46532d050b1c833019c5403cb47
Sharp <c4055518@hallam.shu.ac.uk>
2024-11-06 09:53:18 +0000

    image rotator

--- js/main.js
+++ js/main.js
@@ -4,24 +4,49 @@ document.getElementById("myHeading").innerHTML = "Katy";
 document.querySelector("nav ul li").setAttribute("class", "currentPage");
 document.querySelector("nav ul li a").setAttribute("href", "https://www.google.co.uk");
 
-let colourButtons = document.querySelectorAll(".colPicker");
+//Colour Buttons
+ let colourButtons = document.querySelectorAll(".colPicker");
 console.dir(colourButtons);
 for (let i = 0; i < colourButtons.length; i++) {
-    colourButtons[i+0].style.backgroundColor = "green";
+    colourButtons[i].addEventListener("click", chgColour)
 }
 
 let myButton = document.getElementById("myTestBtn");
 myButton.addEventListener("click", function()
 {
     console.info("hi");
-});
+}); 
 
-let redButton = document.querySelector(".red");
+ let redButton = document.querySelector(".red");
 let greenButton = document.querySelector(".green");
 let blueButton = document.querySelector(".blue");
-let defaultButton = document.querySelector(".reset");
+let defaultButton = document.querySelector(".reset"); 
 
-let documentBody = document.querySelector("body");
+//Image Rotator
+let imageAr = [
+  "images/view1.jpg",
+  "images/view2.jpg",
+  "images/view3.jpg",
+  "images/view4.jpg",
+  "images/view5.jpg",
+  "images/view6.jpg"
+];
+
+let imageCounter = 0;
+let imageRotator = document.getElementById("myImages");
+setInterval(changeImage, );
+
+function changeImage() 
+{
+imageRotator.setAttribute("src", imageAr[imageCounter]);
+imageCounter++; 
+if (imageCounter > imageAr.length)
+    {
+    imageCounter = 0;
+    }
+}
+
+/* let documentBody = document.querySelector("body");
 redButton.addEventListener("click", function() {
     console.info("red clicked");
     documentBody.setAttribute("class", "redBack");
@@ -37,8 +62,8 @@ blueButton.addEventListener("click", function() {
 defaultButton.addEventListener("click", function() {
     console.info("default clicked");
     documentBody.setAttribute("class", "defaultBack");
-});
-
+}); */
+ 
 
 
 
